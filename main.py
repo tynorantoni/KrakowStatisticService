7174fdaa70612b034aa7acb4cfbd79dabdedae16
pawel <pawel.szymaszek@gmail.com>
2020-11-22 02:51:28 +0100

    testing and researching threads and cronjobs

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-import datetime
-
-from pingpong import start
-
-
-def get_json_from_api():
-    pass
+from flask import Flask
+from flask_apscheduler import APScheduler
 
 
+class Config(object):
+    JOBS = [
+        {
+            'id': 'job1',
+            'func': 'main:job1',
+            'args': (1, 2),
+            'trigger': 'interval',
+            'seconds': 10
+        }
+    ]
 
+    SCHEDULER_API_ENABLED = True
+
+
+def job1(a, b):
+    print(str(a) + ' ' + str(b))
 
 if __name__ == '__main__':
+    app = Flask(__name__)
+    app.config.from_object(Config())
 
-    # get_json_from_api()
-    start()
+    scheduler = APScheduler()
+    # it is also possible to enable the API directly
+    # scheduler.api_enabled = True
+    scheduler.init_app(app)
+    scheduler.start()
 
+    app.run()
+
+# from pingpong import start
+#
+# from timeloop import Timeloop
+# from datetime import timedelta
+# import datetime
+#
+# tl = Timeloop()
+#
+# @tl.job(interval=timedelta(seconds=10))
+# def print_jello():
+#     print('hello ',datetime.datetime.now())
+#
+#
+#
+#
+# if __name__ == '__main__':
+#
+#
+#     tl.start(block=True)
+#     start()
+#
+#
