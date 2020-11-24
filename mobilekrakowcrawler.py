fb2aded71968b79c99652e95af4447531173ff56
pawel <pawel.szymaszek@gmail.com>
2020-11-24 18:53:19 +0100

    deploy to travis, RDS

--- mobilekrakowcrawler.py
+++ mobilekrakowcrawler.py
@@ -5,15 +5,15 @@ from selenium import webdriver
 
 from dbmanipulation import insert_to_db
 
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument('--no-sandbox')
-# chrome_options.add_argument('--window-size=1420,1080')
-# chrome_options.add_argument('--headless')
-# chrome_options.add_argument('--disable-gpu')
-# driver = webdriver.Chrome(chrome_options=chrome_options)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--window-size=1420,1080')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(chrome_options=chrome_options)
 
 
-driver = webdriver.Chrome('./chr/chromedriver.exe')
+
 
 
 def get_counters_urls():
--- test_kss.py
+++ test_kss.py
@@ -9,7 +9,17 @@ from selenium import webdriver
 
 from mobilekrakowcrawler import get_street_names, get_counters_urls, dict_of_streets_with_counters_urls
 
+
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--window-size=1420,1080')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(chrome_options=chrome_options)
 driver = webdriver.Chrome('./chr/chromedriver.exe')
+
+
 class TestClass:
 
 
