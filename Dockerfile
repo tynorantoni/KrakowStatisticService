8e67bd456331b60857fa3303a25507f914df4db6
pawel <pawel.szymaszek@gmail.com>
2020-11-25 00:32:13 +0100

    deploy to travis, selenium problem - no chromium

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,28 @@
 FROM python:3.8-alpine3.10
 
 # update apk repo
-#RUN echo "http://dl-4.alpinelinux.org/alpine/v3.10/main" >> /etc/apk/repositories && \
- #   echo "http://dl-4.alpinelinux.org/alpine/v3.10/community" >> /etc/apk/repositories
+RUN echo "http://dl-4.alpinelinux.org/alpine/v3.10/main" >> /etc/apk/repositories && \
+    echo "http://dl-4.alpinelinux.org/alpine/v3.10/community" >> /etc/apk/repositories
 
 # install chromedriver
-#RUN apk update
-#RUN apk add chromium chromium-chromedriver
+RUN apk update
+RUN apk add chromium chromium-chromedriver
+RUN apk --no-cache --update-cache add gcc gfortran build-base wget freetype-dev libpng-dev openblas-dev postgresql-dev
+RUN pip install -U pip
 
-RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-RUN sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-RUN apt-get -y update
-RUN apt-get install -y google-chrome-stable
+#RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+#RUN sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+#RUN apt-get -y update
+#RUN apt-get install -y google-chrome-stable
 
 # install chromedriver
-RUN apt-get install -yqq unzip
-RUN wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
-RUN unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
+#RUN apt-get install -yqq unzip
+#RUN wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
+#RUN unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
 
 # set display port to avoid crash
 ENV DISPLAY=:99
 
-RUN apk --no-cache --update-cache add gcc gfortran build-base wget freetype-dev libpng-dev openblas-dev postgresql-dev
-RUN pip install -U pip
 
 
 
--- mobilekrakowcrawler.py
+++ mobilekrakowcrawler.py
@@ -2,38 +2,26 @@ import datetime
 import re
 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+
 
 from dbmanipulation import insert_to_db
 
 
 
-def set_chrome_options() -> None:
-    """Sets chrome options for Selenium.
-    Chrome options for headless browser is enabled.
-    """
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_prefs = {}
-    chrome_options.experimental_options["prefs"] = chrome_prefs
-    chrome_prefs["profile.default_content_settings"] = {"images": 2}
-    return chrome_options
 
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument('--no-sandbox')
-# chrome_options.add_argument('--window-size=1420,1080')
-# chrome_options.add_argument('--headless')
-# chrome_options.add_argument('--disable-gpu')
-# driver = webdriver.Chrome(chrome_options=chrome_options)
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--window-size=1420,1080')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(chrome_options=chrome_options)
 
 
 
 
 def get_counters_urls():
-    driver = webdriver.Chrome(chrome_options=set_chrome_options())
+
     driver.get("http://mobilnykrakow.pl/rowery/")
     counters = []
     page_source = driver.page_source
@@ -48,7 +36,7 @@ def get_counters_urls():
 
 
 def get_street_names():
-    driver = webdriver.Chrome(options=set_chrome_options())
+
     driver.get("http://mobilnykrakow.pl/rowery/")
     driver.implicitly_wait(5)
     streets = driver.find_elements_by_class_name('title.uppercase.pt-3.pl-3.mb-0')
@@ -67,7 +55,7 @@ def dict_of_streets_with_counters_urls(list_of_counters, list_of_street_names):
 
 
 def get_values_from_counters(dict_of_counters):
-    driver = webdriver.Chrome(options=set_chrome_options())
+
     todays_the_day = datetime.today()
     for count in dict_of_counters:
         driver.get(dict_of_counters[count])
